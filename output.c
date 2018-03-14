fc23636d8f9f629ad03022f925d3f4cb1597f8f4
aimaomao <doriswang1010@outlook.com>
2018-03-14 08:45:34 +0000

    second commit

---
+++ output.c
@@ -0,0 +1,21 @@
+//function definition of evaluation()
+
+#include<stdio.h>
+#include "score.h"
+
+void output_results(const int m[], const int stat[])
+{
+	int i;
+
+	printf("Here are the marks;\n");
+	for(i=0; i<MAX && m[i]!=-1; ++i)
+	{
+		printf ("%4d", m[i]);
+	}
+	printf("\nHere are the Statistics:\n");
+	for (i=0; i<6; i++)
+	{
+		printf(" Mark %d: %d hits\n", i, stat[i]);
+	}
+}
+
